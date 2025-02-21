911525b10e1d3f3f5b2bba03c44e602d972103bf
sadmanryanrad <sadmanryanriad@gmail.com>
2025-02-21 21:38:45 +0600

    event time 'required' removed for frontend flexibility

--- models/Event.js
+++ models/Event.js
@@ -9,10 +9,10 @@ const eventSchema = new mongoose.Schema(
     place: { type: String, trim: true }, // Venue or online (optional)
     isOnline: { type: Boolean, default: false }, // Online or in-person
     joinURL: { type: String }, // Direct join link (optional, useful for Zoom, Google Meet, etc.)
-    eventStartDate: { type: String, required: true }, // Date part of the event start
-    eventStartTime: { type: String, required: true, trim: true }, // Time part of the event start
-    eventEndDate: { type: String, required: true }, // Date part of the event end 
-    eventEndTime: { type: String, required: true, trim: true }, // Time part of the event end
+    eventStartDate: { type: String, trim: true }, // Date part of the event start
+    eventStartTime: { type: String, trim: true }, // Time part of the event start
+    eventEndDate: { type: String, trim: true }, // Date part of the event end 
+    eventEndTime: { type: String, trim: true }, // Time part of the event end
     organizer: { type: String, trim: true }, // Event host (optional)
     category: { type: String, trim: true }, // Workshop, Seminar, etc. (optional)
     eventURL: { type: String, trim: true }, // Link for online events (optional)
